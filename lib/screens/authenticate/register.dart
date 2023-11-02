3b71e0b2cfa185c13d624f22885ea3a7cd9be9f8
amit <amit.singh.9312687983@gmail.com>
2023-11-02 09:21:14 +0530

    Updating the UI

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -49,6 +50,7 @@ class _RegisterState extends State<Register> {
             children: <Widget>[
               const SizedBox(height: 20.0,),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Email"),
                 validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                 onChanged: (val){
                   setState(() {
@@ -59,6 +61,7 @@ class _RegisterState extends State<Register> {
               ),
               const SizedBox(height: 20.0,),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Password"),
                 validator: (val) => val!.length < 6 ? "Enter a password 6+ chars long": null,
                 obscureText: true,
                 onChanged: (val){
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -52,6 +53,7 @@ class _SignInState extends State<SignIn> {
             children: <Widget>[
               const SizedBox(height: 20.0,),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Email"),
                 validator: (val) => val!.isEmpty ? "Enter an email" : null,
                 onChanged: (val){
                   setState(() {
@@ -62,6 +64,7 @@ class _SignInState extends State<SignIn> {
               ),
               const SizedBox(height: 20.0,),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: "Password"),
                 validator: (val) => val!.length <6 ? "Enter a password 6+ chars long": null,
                 obscureText: true,
                 onChanged: (val){
