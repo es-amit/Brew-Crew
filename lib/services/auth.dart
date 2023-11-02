9e54cac4041b9de6b0b44fba5682e556379a0f75
amit <amit.singh.9312687983@gmail.com>
2023-11-02 09:05:06 +0530

    adding Sign in function

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -31,7 +31,20 @@ class AuthService{
       return null;
     }
   }
+
   // sign in with email and password
+  Future signInWithEmailAndPassword(String email,String password) async{
+    try{
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return user;
+
+    }
+    catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
 
   // register with email and password
   Future registerWithEmailAndPassword(String email,String password) async{
